0fedabd408a8f380ab08ab9ff1b64dc7d62a944b
Sriram Balasubramanian <sriramb@iastate.edu>
2016-02-19 13:52:31 -0600

    Added Random hash function

--- src/bloomfilter/BloomFilterRan.java
+++ src/bloomfilter/BloomFilterRan.java
@@ -1,10 +1,34 @@
 package bloomfilter;
 
+import java.math.BigInteger;
+
 public class BloomFilterRan extends BloomFilter {
+	
+	private Integer prime = null;
+	private Integer[] a = null;
+	private Integer[] b = null;
+	
+	BloomFilterRan(int setSize, int bitsPerElement) {
+		super(setSize,bitsPerElement);
+		BigInteger filterSize = BigInteger.valueOf(filterSize());
+		prime = filterSize.nextProbablePrime().intValue();
+		Integer k = numHashes();
+		a = new Integer[k];
+		b = new Integer[k];
+		for(int i=0; i<k ; i++){
+			a[i] = (int) (Math.random() * prime);
+			b[i] = (int) (Math.random() * prime);
+		}
+	}
 	
 	private Integer[] getKHashValues(String s) {
 		Integer[] hashValue = new Integer[numHashes()];
-		
-		return null;
+		BigInteger filterSize = BigInteger.valueOf(filterSize());
+		Integer prime = filterSize.nextProbablePrime().intValue();
+		Integer hash = s.hashCode();
+		for (int i=0;i<numHashes();i++){
+			hashValue[i] = (a[i] + b[i]*hash) % prime;
+		}
+		return hashValue;
 	}
 }
