bbc46985ec1e0bba6ec72f01cc8c487ea682f05e
Sriram Balasubramanian <sriramb@iastate.edu>
2016-02-21 20:12:29 -0600

    Changed hashing

--- src/bloomfilter/FalsePositives.java
+++ src/bloomfilter/FalsePositives.java
@@ -1,5 +1,7 @@
 package bloomfilter;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
@@ -8,45 +10,57 @@ public class FalsePositives {
 	
 	private BloomFilter bloomFilter;
 	private Set<String> inputSet;
+	private Set<String> insertedSet;
+	private Set<String> notInsertedSet;
 	
 	
 	public FalsePositives(BloomFilter bloomFilter) {
 		this.bloomFilter = bloomFilter;
 		this.inputSet = new TreeSet<String>();
+		this.insertedSet = new TreeSet<String>();
+		this.notInsertedSet = new TreeSet<String>();
 	}
 	
 	public float getFalsePositiveRate(){
 		
 		float fpCount = 0;
-		int bFilterLength = 0;
 		int searchPositives = 0;
 		//create input list 
-		permutation("","abcdefghi");
-		System.out.println("Input Set Size - "+inputSet.size());
+		//permutation("","abcdefghi");
+		randomStrings(50000);
 		//Insert into bloom filter only alternative elements
 		Iterator<String> setIterator = inputSet.iterator();
 		int i=0;
 		while(setIterator.hasNext()){
 			String input = setIterator.next();
 			if(i%2 == 0){
+				insertedSet.add(input);
 				bloomFilter.add(input);
-				bFilterLength++;
+			} else {
+				notInsertedSet.add(input);
 			}
 			i++;
 		}
 		//Run through the list again and check for presense of all elements
-		setIterator = inputSet.iterator();
+		setIterator = notInsertedSet.iterator();
 		while(setIterator.hasNext()){
 			if(bloomFilter.appears(setIterator.next())){
 				searchPositives++;
 			}
 		}
 		//calculate False Positives
-		System.out.println("Search Positives - "+searchPositives);
-		System.out.println("Filter Length - "+bFilterLength);
-		fpCount = searchPositives - bFilterLength;
-		fpCount = fpCount/searchPositives;
+		System.out.println("False Positives - "+searchPositives);
+		System.out.println("Input Length - "+notInsertedSet.size());
+		fpCount = (float)searchPositives/notInsertedSet.size();
 		return fpCount;
+	}
+
+	private void randomStrings(int i) {
+		SecureRandom random = new SecureRandom();
+		while(inputSet.size() < i) {
+			inputSet.add(new BigInteger(130, random).toString(32));
+		}
+		
 	}
 
 	public int getInputListSize(){
@@ -55,22 +69,15 @@ public class FalsePositives {
 		}
 		return -1;
 	}
-
-	private void permutation(String prefix, String str) {
-	    int n = str.length();
-	    if (n == 0) inputSet.add(prefix);
-	    else {
-	        for (int i = 0; i < n; i++)
-	            permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
-	    }
-	}
 	
 	public static void main(String[] args) {
-		FalsePositives fPositiveFNV = new FalsePositives(new BloomFilterDet(100000,4));
-		System.out.println(fPositiveFNV.getFalsePositiveRate());
+		System.out.println("Determinitic Bloom filter:");
+		FalsePositives fPositiveFNV = new FalsePositives(new BloomFilterDet(50000,1));
+		System.out.println("False Positive rate - " + fPositiveFNV.getFalsePositiveRate());
 		
-		FalsePositives fPositiveRND = new FalsePositives(new BloomFilterRan(100000,4));
-		System.out.println(fPositiveRND.getFalsePositiveRate());
+		System.out.println("Random Bloom filter:");
+		FalsePositives fPositiveRND = new FalsePositives(new BloomFilterRan(50000,1));
+		System.out.println("False Positive rate - " + fPositiveRND.getFalsePositiveRate());
 		
 		
 		
